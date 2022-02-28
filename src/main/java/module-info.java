439bcf676380f40978c40f03b89d048cba11c17f
super-cow-powers <dm1306@york.ac.uk>
2022-02-28 22:14:42 +0000

    Add XOM to deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module g3.project.core {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires nu.xom;
+    
     opens g3.project.core to javafx.fxml;
     exports g3.project.core;
 }
